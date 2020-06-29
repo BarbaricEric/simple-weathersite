b03bdf96e9c8f1c0c54434897ca4f4efd5769f93
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-06-29 05:10:53 -0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', ()=> {
+ let long;
+ let lat;
+ const temperatureDescription = document.querySelector(".temperature-description");
+ const temperatureDegree = document.querySelector(".temperature-degree");
+ const locationTimezone = document.querySelector(".location-timezone");
+ const temperatureSection = document.querySelector(".temperature");
+ const temperatureSpan = document.querySelector(".temperature span");
