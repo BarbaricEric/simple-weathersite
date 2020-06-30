915f2e08e512320a749081c9c6b7e7b7c6b9f25f
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-06-30 05:10:06 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Define Global Terms to use
 window.addEventListener('load', ()=> {
  let long;
  let lat;
@@ -6,3 +7,14 @@ window.addEventListener('load', ()=> {
  const locationTimezone = document.querySelector(".location-timezone");
  const temperatureSection = document.querySelector(".temperature");
  const temperatureSpan = document.querySelector(".temperature span");
+
+ // Main Function
+ if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(position => {
+  long = position.coords.longitude;
+  lat = position.coords.latitude;
+
+   const proxyServer = 
+   const api = 
+    
+  });
